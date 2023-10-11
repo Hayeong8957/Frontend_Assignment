836a73cfb5faf2c87c15e062fe1a1d637b45c0de
HayeongShin <judy.hayoung.shin@gmail.com>
2023-10-12 02:47:53 +0900

    chore : 프로젝트 세팅

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Frontend_Assignment
+
+# Tech Stack
+
+- TypeScript
+- Next.js, React
+- React Query
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ styles/color.ts
@@ -0,0 +1,20 @@
+const color = {
+  black: '#000000',
+  fontblack: '#16191f',
+  fontgray: '#687078',
+  navborder: '#d5dbdb',
+  inputborder: '#85909b',
+
+  gray1: '#333333',
+  gray2: '#4f4f4f',
+  gray3: '#828282',
+  gray4: '#bdbdbd',
+  gray5: '#e0e0e0',
+  gray6: '#f2f2f2',
+  searchBlue: '#0e386b',
+  totalResultBlue: '#aff',
+  border: '#eaeded',
+  headergray: '#fafafa',
+};
+
+export default color;
